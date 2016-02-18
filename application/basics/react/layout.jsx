085faadd761e52b753d4e061ba43e8780a61e48a
prizerok <prizerok@gmail.com>
2016-02-18 20:27:23 +0300

    test 1 inless init

---
+++ application/basics/react/layout.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+export default {
+	render() {
+		return (
+			<main>
+				{this.props.children}
+			</main>
+		);
+	}
+}
---
+++ application/components/header/react/index.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+// import comp from 'components';
+// var Header = comp('Header');
+
+export default {
+	render() {
+		return <div className="comp-%name%">Component %name%</div>;
+	}
+}
